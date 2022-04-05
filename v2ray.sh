cbd932a9b212b9a7b8251d2eb441abe152fdebc0
大黄蜂coder <chinesezwf@gmail.com>
2022-04-05 12:10:12 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+id=$(cat /proc/sys/kernel/random/uuid)
+path=$(cat /proc/sys/kernel/random/uuid)
+dockerDataPath="/app/dockerTest"
+v2rayPort=44444
+
 # 安装docker
 installDocker() {
 	docker >/dev/null 2>&1
@@ -10,16 +15,56 @@ installDocker() {
 		systemctl enable docker.service
 		systemctl start docker
 	fi
+	start_menu
+}
 
+# 安装v2ray
+installV2ray() {
+	mkdir -p ${dockerDataPath}/v2ray
+	cat <<EOF >${dockerDataPath}/v2ray/config-ws.json
+{
+    "inbounds": [
+        {
+            "port": ${v2rayPort},
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${id}",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/${path}"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+	start_menu
 }
 
 #开始菜单
 start_menu() {
 	clear
 	echo && echo -e " v2ray 一键安装管理脚本
+id : ${id}
+path : ${path}
 ————————————脚本列表————————————
 0. 退出脚本
-1. 安装 docker
+1. 安装docker
+2. 安装v2ray
 ————————————————————————————————" && echo
 	echo
 	read -p " 请输入数字:" num
@@ -30,10 +75,12 @@ start_menu() {
 	1)
 		installDocker
 		;;
+	2)
+		installV2ray
+		;;
 	*)
 		clear
 		echo -e "请输入正确数字"
-		sleep 5s
 		start_menu
 		;;
 	esac
